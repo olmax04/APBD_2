147d6b65ae607d236a526dde9d49d55091933723
Olmax04[Zenbook] <olmax04developer@gmail.com>
2024-03-11 15:04:34 +0100

    Edit method 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,7 +15,7 @@ static double CalculateAverage(int[] array)
     int sum = 0;
     for(int i = 0; i < array.Length; i++)
     {
-        sum += array[i];
+        summ += array[i];
     }
     return (double)sum / array.Length;
 }
