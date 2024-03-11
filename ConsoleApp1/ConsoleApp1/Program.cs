58dccb11594bc75bdbc3127309f677b965118e31
Olmax04[Zenbook] <olmax04developer@gmail.com>
2024-03-11 14:33:28 +0100

    For loop counting

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,3 +4,8 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+for (int i = 0; i < 5; i++) 
+{
+    Console.WriteLine(i);
+}
