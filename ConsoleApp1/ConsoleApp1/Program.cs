04ba209b81e9ce1b36f796fd7499be2ae801408c
Olmax04[Zenbook] <olmax04developer@gmail.com>
2024-03-11 14:44:16 +0100

    Before Merge

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,3 +4,21 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+for (int i = 0; i < 5; i++) 
+{
+    Console.WriteLine(i);
+}
+
+static double CalculateAverage(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return (double)sum / array.Length;
+}
+
+int[] array = {1, 2, 3, 4, 5};
+Console.WriteLine(CalculateAverage(array));
