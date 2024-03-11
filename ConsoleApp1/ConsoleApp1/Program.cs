c3dfcf6cfda98780fb7f5f2f8b6b80ca2e327180
Olmax04[Zenbook] <olmax04developer@gmail.com>
2024-03-11 14:37:45 +0100

    Nowa metoda

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,3 +9,16 @@ for (int i = 0; i < 5; i++)
 {
     Console.WriteLine(i);
 }
+
+static double CalculateAverage(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        sum += array[i];
+    }
+    return (double)sum / array.Length;
+}
+
+int[] array = {1, 2, 3, 4, 5};
+Console.WriteLine(CalculateAverage(array));
